1ec9d1c4a502326383a9a286b927015ef9aad1ed
Christina Blakely <ceb51@students.uwf.edu>
2017-11-27 06:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ const newID = 1000000000 + oldID;
 const ageIsValid = Number.isInteger(21.7);
 const randomNumber = Math.random() * 20;
 const randomInteger = Math.floor(randomNumber);
+const randomUserID = 1000000001 + randomInteger; 
